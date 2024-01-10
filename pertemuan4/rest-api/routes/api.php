d0ae6f01babb4f33abb61f36ba542b951a4df826
AbrarAlArkan <abrararkan403@gmail.com>
2024-01-10 18:02:27 +0700

    menambahkan tugas pertemuan 5

--- pertemuan4/rest-api/routes/api.php
+++ pertemuan4/rest-api/routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\AnimalController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+# mengimport controller Student
+use App\Http\Controllers\StudentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +20,16 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+# Route animals
+Route::get('/animals', [AnimalController::class, 'index']);
+Route::post('/animals', [AnimalController::class, 'store']);
+Route::put('/animals/{id}', [AnimalController::class, 'update']);
+Route::delete('/animals/{id}', [AnimalController::class, 'destroy']);
+
+# Route students
+# Method GET
+Route::get('/students', [StudentController::class, 'index']);
+
+# Method POST
+Route::post('/students', [StudentController::class, 'store']);
